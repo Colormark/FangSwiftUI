240bbdb89ac3ebc53ed7c9b0b170145bf639f1a0
Colormark <10973837@qq.com>
2015-12-28 11:48:46 +0800

    Update UIConfig.swift

--- UIConfig.swift
+++ UIConfig.swift
@@ -1,10 +1,3 @@
-//
-//  AppUIConfig.swift
-//  studentdayi
-//
-//  Created by FangYan on 15/12/7.
-//  Copyright © 2015年 yousi.inc. All rights reserved.
-//
 /*
 ****使用说明****
 
@@ -14,109 +7,167 @@
 3. 通常辅助色（20~30%）与主色相临近或互补
 4. 点缀色（点缀色 5~8%）一般用饱和度和明亮度高的颜色
 5. 通常主色深也是品牌色，主色浅是主导色，当然主导色可以与品牌色完全一样
-6. 强调色指需要特殊强调，与主色等区别比较明显的颜色
+6. 强调色指需要特殊强调，与主色等区别比较明显的颜色，鲜艳的强调色用于你的主要操作按钮以及组件，如开关或滑片。左对齐的部分图标或章节标题也可以使用强调色。
 
-==色彩名说明==
-1. 同一色的临近表达建议只用alpha，临近表达不要超过3色
+==How==
+
+UI.Color["Primary"]
+
 */
 
-let YSUI : Dictionary <String,AnyObject> = [
+public struct UI {
     
-    "navigationBar":[
-        "backgroundColor" : YSUIColor["primary"]!,
-        "textColor" : YSUIColor["white"]!
-    ],
-    
-    
-    "card":[
-        "backgroundColor" : YSUIColor["white"]!,
+    static let Color: Dictionary <String,UIColor> = [
         
-        "titleColor" : YSUIColor["textPrimary"]!,
-        "titleSize" : YSUISize["XL"]!,
+        "Primary"   : UIColor(red: 0x00, green: 0xD8/255, blue: 0xD8/255, alpha: 1.0),        //主色。
+        "Secondary" : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 1.0),    //辅助色，次色
         
-        "subTitleColor" : YSUIColor["textPrimary"]!,
-        "subTitleSize" : YSUISize["NM"]!,
+        "AccentA"   : UIColor(red: 0xEE/255, green: 0xEE/255, blue: 0xEE/255, alpha: 1.0),    //强调色A。
+        "AccentB"   : UIColor(red: 0xF6/255, green: 0xF6/255, blue: 0xF6/255, alpha: 1.0),    //强调色B
+        "AccentC"   : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 0.96),   //强调色C，三个强调色，一般来说有点多
         
-        "summaryTextColor" : YSUIColor["textLight"]!,
-        "summaryTextSize" : YSUISize["NM"]!,
+        "Warning"   : UIColor(red: 0xFF/255, green: 0x68/255, blue: 0x00/255, alpha: 0.96), //警告
+        "Danger"    : UIColor(red: 0xFF/255, green: 0x68/255, blue: 0x00/255, alpha: 0.96), //错误、危险
+        "Success"   : UIColor(red: 0xFF/255, green: 0x68/255, blue: 0x00/255, alpha: 0.96), //操作成功、完成
         
-        "PanelTextColor" : YSUIColor["textSecondary"]!,
-        "PanelTextSize" : YSUISize["SM"]!,
-        "PanelBorderColor" : YSUIColor["linelight"]!,
-        "PanelBorderSize" : 1,
         
-        "avatarBorderColor" : YSUIColor["black2"]!,
-        "avatarBorderSize" : 3,
+        /*
+        * 线、分割线
+        */
+        "LineDark"  : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.20), //深
+        "LineLight" : UIColor(red: 0xE2/255, green: 0xE3/255, blue: 0xE4/255, alpha: 1.0), //浅
+        
+        
+        /*
+        * 黑白，灰度 用色
+        * 通常用于背景、文字、分割线用色，请尽量不要修改
+        */
+        "White"       :  UIColor.whiteColor(),
+
+        "Black50"     :  UIColor(red: 0xFA/255, green: 0xFA/255, blue: 0xFA/255, alpha: 1.0),
+        "GhostWhite"  :  UIColor(red: 0xFA/255, green: 0xFA/255, blue: 0xFA/255, alpha: 1.0),
+        
+        "Black100"    :  UIColor(red: 0xF5/255, green: 0xF5/255, blue: 0xF5/255, alpha: 1.0),
+        "WhiteSmoke"  :  UIColor(red: 0xF5/255, green: 0xF5/255, blue: 0xF5/255, alpha: 1.0),
+        
+        "Black200"    :  UIColor(red: 0xE6/255, green: 0xE6/255, blue: 0xE6/255, alpha: 1.0),
+        "Mercury"     :  UIColor(red: 0xF5/255, green: 0xF5/255, blue: 0xF5/255, alpha: 1.0),
+        
+        "Black200"    :  UIColor(red: 0xCC/255, green: 0xCC/255, blue: 0xCC/255, alpha: 1.0),
+        "Siliver"     :  UIColor(red: 0xCC/255, green: 0xCC/255, blue: 0xCC/255, alpha: 1.0),
+        
+        "Black300"    :  UIColor(red: 0xB3/255, green: 0xB3/255, blue: 0xB3/255, alpha: 1.0),
+        "Magnesium"   :  UIColor(red: 0xB3/255, green: 0xB3/255, blue: 0xB3/255, alpha: 1.0),
+        
+        "Black400"    :  UIColor(red: 0x99/255, green: 0x99/255, blue: 0x99/255, alpha: 1.0),
+        "Aluminum"    :  UIColor(red: 0x99/255, green: 0x99/255, blue: 0x99/255, alpha: 1.0),
+        
+        "Black500"    :  UIColor(red: 0x80/255, green: 0x80/255, blue: 0x80/255, alpha: 1.0),
+        "Nickel"      :  UIColor(red: 0x80/255, green: 0x80/255, blue: 0x80/255, alpha: 1.0),
+        
+        "Black600"    :  UIColor(red: 0x7F/255, green: 0x7F/255, blue: 0x7F/255, alpha: 1.0),
+        "Tin"         :  UIColor(red: 0x7F/255, green: 0x7F/255, blue: 0x7F/255, alpha: 1.0),
+        
+        "Black700"    :  UIColor(red: 0x66/255, green: 0x66/255, blue: 0x66/255, alpha: 1.0),
+        "Steel"       :  UIColor(red: 0x66/255, green: 0x66/255, blue: 0x66/255, alpha: 1.0),
+        
+        "Black800"    :  UIColor(red: 0x4C/255, green: 0x4C/255, blue: 0x4C/255, alpha: 1.0),
+        "Iron"        :  UIColor(red: 0x4C/255, green: 0x4C/255, blue: 0x4C/255, alpha: 1.0),
+            
+        "Black900"    :  UIColor(red: 0x33/255, green: 0x33/255, blue: 0x33/255, alpha: 1.0),
+        "Tungsten"    :  UIColor(red: 0x33/255, green: 0x33/255, blue: 0x33/255, alpha: 1.0),
+            
+        "Black950"    :  UIColor(red: 0x19/255, green: 0x19/255, blue: 0x19/255, alpha: 1.0),
+        "Lead"        :  UIColor(red: 0x19/255, green: 0x19/255, blue: 0x19/255, alpha: 1.0),
+        
+        "Black"       : UIColor.blackColor(),
+        
+        /*
+        * 黑白透明
+        */
+        "WhiteAlphaLight"      : UIColor(red: 255, green: 255, blue:255, alpha: 0.88), //浅
+        "WhiteUltraAlphaLight" : UIColor(red: 255, green: 255, blue: 255, alpha: 0.32), //很浅
+        
+        "BlackAlphaLight"      : UIColor(red: 0, green: 0, blue: 0, alpha: 0.88), //浅
+        "BlackUltraAlphaLight" : UIColor(red: 0, green: 0, blue: 0, alpha: 0.32), //很浅
+        
     ]
     
-]
+    let TextColor: Dictionary <String,UIColor> = [
+        
+        "Defalut" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.87),//对于白色背景上的文字，标准 alpha 值为 87%
+        "Dark" :    UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.54),//视觉层次偏低的次要文字，应该使用 54%
+        "Light" :   UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.26),//正文和标签中用于提示用户的文字，视觉层次更低，应该使用26%
+        "Accent" :  Color["Primary"]!, //使用主色
+        
+    ]
+    
+    static let TextSize: Dictionary <String,CGFloat> = [
+        "XXXL":42,       //Display1
+        "XXL":34,        //Display2
+        "XL":24,         //大标题 Headline
+        "LG":20,         //Appbar
+        "Normal":16,     //小标题 Subhead
+        "SM":14,         //正文 按钮文字 Body Button
+        "XS":12,         //小字提示
+        "XXS":11         //Display3
+    ]
+    
+    static let Layout: Dictionary <String,CGFloat> = [
+        
+        "Type"       :   4,         //最小距离和尺寸单元，排版/文字（Type）与间隔为 4dp 的基准网格对齐。在工具条中的图标同样与间隔为 4dp 的基准网格对齐。
+        
+        "SpaceSM"    :   8,         //所有组件都与间隔为 8dp 的基准网格对齐，
+        "Space"      :   16,
+        "SpaceLG"    :   32,
+        "SpaceXL"    :   48,
+        
+        "HSpaceSM"    :   10,         //iOS系统，水平间隔的最小单位为10，
+        "HSpace"      :   20,
+        "HSpaceLG"    :   36,
+        "HSpaceXL"    :   48,
+        
+        "IconSM"     :   16,
+        "Icon"       :   24,
+        "IconLG"     :   32,
+        "IconXL"     :   40,
+        "IconXXL"    :   56,
+        "IconXXXL"   :   64
+        
+    ]
+    
+    static let ButtonSize: Dictionary <String,CGFloat> = [
+        "XL"         : 28,
+        "LG"         : 18,
+        "Normal"     :14,
+        "SM"         :12
+    ]
+    
+    static let Radius: Dictionary <String,CGFloat> = [
+        "None"       :0,
+        "Tiny"       :0.15,
+        "Normal"     :0.24,
+        "Round"      :0.5,
+        "Cricle"     :1
+    ]
+    
+    
+    struct Shadow {
+        
+        static let offset: Dictionary <String,CGSize> = [
+            "Tiny"       :CGSizeMake(0,1),
+            "Normal"     :CGSizeMake(1,2),
+            "Strong"     :CGSizeMake(4,4),
+            "Huge"       :CGSizeMake(3,3)
+        ]
+        
+        static let Color: Dictionary <String,UIColor> = [
+            "Dark"     : UIColor(red: 0, green: 0, blue: 0, alpha: 0.58),
+            "Light"     : UIColor(red: 0, green: 0, blue: 0, alpha: 0.32)
+        ]
+        
+        
+    }
 
-let YSUIColor:Dictionary <String,UIColor> = [
-    /*
-    * 主配色
-    */
-    "primary" : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 0.96), //主色深
-    "primaryLight" : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 0.82), //主色浅
-
-    "secondaryA" : UIColor(red: 0xEE/255, green: 0xEE/255, blue: 0xEE/255, alpha: 1.0), //辅助色A
-    "secondaryB" : UIColor(red: 0xF6/255, green: 0xF6/255, blue: 0xF6/255, alpha: 1.0), //辅助色B
-    "secondaryC" : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 0.96), //辅助色C
     
-    "highlight" : UIColor(red: 0x35/255, green: 0xE7/255, blue: 0x45/255, alpha: 1.0), //高亮色
-    "accent" : UIColor(red: 0x49/255, green: 0xBB/255, blue: 0xCD/255, alpha: 0.96), //点缀色、强调色
-    
-    /*
-    * 文字色
-    */
-    "textPrimary" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.88), //主色，建议黑或近黑
-    "textSecondary" : UIColor(red: 0x6A/255, green: 0x6D/255, blue: 0x7B/255, alpha: 1.0), //辅助色，建议黑或近黑
-    "textLight" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.70), //主色的light色
-    "textHighlight" : UIColor(red: 0x5E/255, green: 0xB8/255, blue: 0xFA/255, alpha: 1.0), //高亮色
-    
-    
-    /*
-    * 线、分割线
-    */
-    "lineDark" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.20), //深
-    "linelight" : UIColor(red: 0xE2/255, green: 0xE3/255, blue: 0xE4/255, alpha: 1.0), //浅
-    
-    /*
-    * 白色
-    */
-    "white" : UIColor(red: 0xFF/255, green: 0xFF/255, blue: 0xFF/255, alpha: 1.0), //深
-    "whiteLight" : UIColor(red: 0xFF/255, green: 0xFF/255, blue: 0xFF/255, alpha: 0.88), //浅
-    
-    /*
-    * 黑色
-    */
-    "black"  : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 1.0),
-    "black8" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.8),
-    "black6" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.6),
-    "black4" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.4),
-    "black3" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.3),
-    "black2" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.2),
-    "black1" : UIColor(red: 0x0/255, green: 0x0/255, blue: 0x0/255, alpha: 0.1),
-    
-]
-
-let YSUISize:Dictionary <String,Float> = [
-    "XXL":36,
-    "XL":32,
-    "LG":28,
-    "NM":24,
-    "SM":22,
-    "XS":18,
-]
-
-let YSUIShadowSize:Dictionary <String,CGSize> = [
-    "LG":CGSizeMake (3.0, 3.0),
-    "NM":CGSizeMake (0, 2.0),
-    "SM":CGSizeMake (0, 1.0)
-]
-
-let YSUIShadowRadius:Dictionary <String,CGSize> = [
-    "LG":CGSizeMake (3.0, 3.0),
-    "NM":CGSizeMake (0, 2.0),
-    "SM":CGSizeMake (0, 1.0)
-]
-
+}
